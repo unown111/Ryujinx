f4945e76818ff677bc8e6642545671318b12ca5e
gdkchan <gab.dark.100@gmail.com>
2020-01-03 00:11:27 -0300

    Proper generic constraints

--- Ryujinx.HLE/HOS/Services/Hid/Hid.cs
+++ Ryujinx.HLE/HOS/Services/Hid/Hid.cs
@@ -9,7 +9,7 @@ namespace Ryujinx.HLE.HOS.Services.Hid
         private readonly Switch _device;
         private ulong _hidMemoryAddress;
 
-        internal ref HidSharedMemory SharedMemory => ref _device.Memory.GetRef<HidSharedMemory>(_hidMemoryAddress, Horizon.HidSize);
+        internal ref HidSharedMemory SharedMemory => ref _device.Memory.GetRef<HidSharedMemory>(_hidMemoryAddress);
         internal const int SharedMemEntryCount = 17;
 
         public DebugPadDevice DebugPad;
